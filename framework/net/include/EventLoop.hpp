18b93be6c79b47d7eb497be2cd853e2cf3986f11
SmileLionLY <1252451603@qq.com>
2018-05-07 21:36:59 +0800

    add eventloop

---
+++ framework/net/include/EventLoop.hpp
@@ -0,0 +1,59 @@
+#ifndef FRAMEWORK_NET_INCLUDE_EVENTLOOP_HPP_
+#define FRAMEWORK_NET_INCLUDE_EVENTLOOP_HPP_
+
+#include <nocopyable.hpp>
+
+namespace Net {
+
+class Channel;
+class Poller;
+
+class EventLoop : Base::nocopyable {
+ public:
+    typedef std::function<void()> Task;
+
+    EventLoop();
+
+    ~EventLoop();
+
+    void Loop();
+
+    void Quit();
+
+    void RunInLoop(Task task);
+
+    void QueueInLoop(Task task);
+
+    int QueueSize();
+
+    // channel about
+    void UpdateChannel(Channel *channel);
+
+    void RemoveChannel(Channel *channel);
+
+    bool HasChannel(Channel *channel);
+
+    void PrintActiveChannels();
+
+ private:
+    void WakeUP();
+
+    void HandleRead();
+
+ private:
+    typedef std::vector<Channel *> ChannelList;
+
+    std::unique_ptr<Poller> poller_;
+
+    int wakeup_fd_;
+
+    std::unique_ptr<Channel> wakeup_channel_;
+
+    ChannelList active_channels_;
+
+    Channel *current_active_channel_;
+};
+
+}
+
+#endif
